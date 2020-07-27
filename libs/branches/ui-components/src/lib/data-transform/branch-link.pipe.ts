171f7cc5733fb24b031b785d62649779d34bd458
Mark Goho <markgoho@gmail.com>
2020-07-27 12:37:01 -0400

    build entire branch link via pipe

--- libs/branches/ui-components/src/lib/data-transform/branch-link.pipe.ts
+++ libs/branches/ui-components/src/lib/data-transform/branch-link.pipe.ts
@@ -1,10 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { BranchInfo } from '@idc/branches/data-access';
 
 @Pipe({
   name: 'branchLink',
 })
 export class BranchLinkPipe implements PipeTransform {
-  transform(value: string): string {
-    return encodeURI(value);
+  transform(branch: BranchInfo): string {
+    const query = encodeURI(branch.branchName);
+
+    return `//github.com/${branch.organizationName}/${branch.repositoryName}/branches/all?query=${query}`;
   }
 }
