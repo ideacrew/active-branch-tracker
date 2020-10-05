941182526958ade6e5c094cc06fa427e530af7a0
Mark Goho <markgoho@gmail.com>
2020-10-05 16:10:04 -0400

    another tweak to admin

--- functions/src/grant-admin.ts
+++ functions/src/grant-admin.ts
@@ -1,7 +1,6 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
 
-const firestore = admin.firestore();
+import { grantAdminRole } from './util/grantAdmin';
 
 export async function enableAdminForUser(
   data: any,
