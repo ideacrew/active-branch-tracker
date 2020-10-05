a2888686f0da08e316fca1c7e171354038f49279
Mark Goho <markgoho@gmail.com>
2020-10-05 14:18:23 -0400

    update nrwl workspace

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest'
-  },
-  resolver: '@nrwl/jest/plugins/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html']
+  projects: [
+    '<rootDir>/apps/dashboard',
+    '<rootDir>/libs/branches/data-access',
+    '<rootDir>/libs/branches/feature',
+    '<rootDir>/libs/branches/ui-components',
+    '<rootDir>/libs/display-config',
+    '<rootDir>/libs/util',
+    '<rootDir>/libs/environments/data-access',
+    '<rootDir>/libs/environments/feature',
+    '<rootDir>/libs/testing',
+    '<rootDir>/libs/auth',
+    '<rootDir>/libs/user/data-access',
+    '<rootDir>/libs/user/feature',
+    '<rootDir>/libs/google-analytics',
+  ],
 };
