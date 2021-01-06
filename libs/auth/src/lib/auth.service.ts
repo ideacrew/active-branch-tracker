4a951c58f5b6ebeed688c6f18283c461c5aa86b9
Mark Goho <markgoho@gmail.com>
2021-01-06 12:43:07 -0500

    update import statements

--- libs/auth/src/lib/auth.service.ts
+++ libs/auth/src/lib/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth, User } from 'firebase/app';
+import firebase from 'firebase/app';
+
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,15 +10,15 @@ import { Observable } from 'rxjs';
 export class AuthService {
   constructor(public afAuth: AngularFireAuth) {}
 
-  login(): Promise<auth.UserCredential> {
-    return this.afAuth.signInWithPopup(new auth.GoogleAuthProvider());
+  login(): Promise<firebase.auth.UserCredential> {
+    return this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
   }
 
   logout(): Promise<void> {
     return this.afAuth.signOut();
   }
 
-  get user$(): Observable<User | null> {
+  get user$(): Observable<firebase.User | null> {
     return this.afAuth.user;
   }
 }
