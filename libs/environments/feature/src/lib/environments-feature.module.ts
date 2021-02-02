d33475daf17cfabaa8c93ab7d4dc11fdb6f20d5b
Mark Goho <markgoho@gmail.com>
2021-02-02 11:49:23 -0500

    add org list to environments landing page

--- libs/environments/feature/src/lib/environments-feature.module.ts
+++ libs/environments/feature/src/lib/environments-feature.module.ts
@@ -4,16 +4,17 @@ import { RouterModule } from '@angular/router';
 
 import { EnvironmentsDataAccessModule } from '@idc/environments/data-access';
 import { EnvironmentsComponent } from './environments/environments.component';
+import { OrgListComponent } from './org-list/org-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
 
     RouterModule.forChild([
-      { path: '', pathMatch: 'full', component: EnvironmentsComponent },
+      { path: '', pathMatch: 'full', component: OrgListComponent },
     ]),
     EnvironmentsDataAccessModule,
   ],
-  declarations: [EnvironmentsComponent],
+  declarations: [EnvironmentsComponent, OrgListComponent],
 })
 export class EnvironmentsFeatureModule {}
---
+++ libs/environments/feature/src/lib/org-list/org-list.component.ts
@@ -0,0 +1,8 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  templateUrl: './org-list.component.html',
+  styleUrls: ['./org-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class OrgListComponent {}
