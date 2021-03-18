e83879d85e384010d5d5eb68779eb7adbfdec57c
Mark Goho <markgoho@gmail.com>
2021-03-18 16:42:33 -0400

    begin testing http functions

--- functions/test/functions.spec.js
+++ functions/test/functions.spec.js
@@ -1,113 +1,72 @@
 const { expect } = require('chai');
+const axios = require('axios');
+const qs = require('qs');
 const admin = require('firebase-admin');
 const test = require('firebase-functions-test')({
   projectId: process.env.GCLOUD_PROJECT,
 });
-const myFunctions = require('../lib/index');
+
+const axiosConfig = (functionName, data) => {
+  return {
+    method: 'post',
+    url: `http://localhost:5001/${process.env.GCLOUD_PROJECT}/us-central1/${functionName}`,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    data,
+  };
+};
 
 describe('Unit tests', () => {
   after(() => {
     test.cleanup();
   });
 
-  it.skip('tests a new deployment', async () => {
-    const payload = `{"branch": "epic-37695", "env": "pvt-2", "app": "enroll", "user_name": "kvootla", "commit_sha": "4de7a43", "org": "dchbx", "repo": "enroll" }`;
-    const req = { body: { payload } };
-
-    const sendPromise = new Promise(resolve => {
-      // A fake response object, with a stubbed send() function which asserts that it is called
-      // with the right result
-      const res = {
-        send: text => {
-          resolve(text);
-        },
-      };
-
-      // Invoke function with our fake request and response objects.
-      myFunctions.branchDeployment(req, res);
+  it('tests a new deployment', async () => {
+    const data = qs.stringify({
+      payload:
+        '{"status": "started", "branch": "feature-fix", "env": "hotfix-2", "app": "enroll", "user_name": "kvootla", "org": "maine", "repo": "enroll", "commit_sha": "abc1234" }',
     });
+
+    const config = axiosConfig('branchDeployment', data);
+
+    let responsePayload;
+    try {
+      // Make the http request
+      responsePayload = await axios(config);
+    } catch (e) {
+      console.log('ERROR:', e);
+    }
 
     // Wait for the promise to be resolved and then check the sent text
-    const responsePayload = await sendPromise;
-    expect(responsePayload).to.deep.eq({
-      branch: 'epic-37695',
-      env: 'pvt-2',
+    expect(responsePayload.data).to.deep.eq({
+      status: 'started',
+      branch: 'feature-fix',
+      env: 'hotfix-2',
       app: 'enroll',
       user_name: 'kvootla',
-      commit_sha: '4de7a43',
-      org: 'dchbx',
+      org: 'maine',
       repo: 'enroll',
+      commit_sha: 'abc1234',
     });
 
-    const envSnap = await admin
-      .firestore()
-      .collection('environments')
-      .doc('dchbx-pvt-2')
-      .get();
-    expect(envSnap.data()).to.eql({
-      latestDeployment: {
-        branch: 'epic-37695',
-        env: 'pvt-2',
-        app: 'enroll',
-        user_name: 'kvootla',
-        commit_sha: '4de7a43',
-        org: 'dchbx',
-        repo: 'enroll',
-      },
-    });
+    // const envSnap = await admin
+    //   .firestore()
+    //   .collection('orgs')
+    //   .doc('maine')
+    //   .collection('environments')
+    //   .doc('hotfix-2')
+    //   .get();
 
-    const branchSnap = await admin
-      .firestore()
-      .collection('branches')
-      .doc('dchbx-epic-37695')
-      .get();
-
-    expect(branchSnap.data()).to.include({ environment: 'pvt-2' });
-
-    expect;
-  }).timeout(5000);
-
-  it('should test updates to environment document', async () => {
-    const wrapped = test.wrap(myFunctions.watchEnvironments);
-
-    const beforeSnap = test.firestore.makeDocumentSnapshot(
-      {
-        latestDeployment: {
-          branch: 'branch-one',
-          env: 'env-1',
-          app: 'app',
-          user_name: 'kvootla',
-          commit_sha: 'sha1',
-          org: 'org-one',
-          repo: 'app',
-        },
-      },
-      'environments/org-env1',
-    );
-    const afterSnap = test.firestore.makeDocumentSnapshot(
-      {
-        latestDeployment: {
-          branch: 'branch-two',
-          env: 'env-1',
-          app: 'app',
-          user_name: 'kvootla',
-          commit_sha: 'sha2',
-          org: 'org-one',
-          repo: 'app',
-        },
-      },
-      'environments/org-env1',
-    );
-
-    const change = test.makeChange(beforeSnap, afterSnap);
-    await wrapped(change);
-
-    const branchOneSnap = await admin
-      .firestore()
-      .collection('branches')
-      .doc('org-one-branch-one')
-      .get();
-
-    expect(branchOneSnap.data()).to.not.haveOwnProperty('environment');
+    // expect(envSnap.data()).to.eql({
+    //   status: 'started',
+    //   branch: 'feature-fix',
+    //   env: 'hotfix-2',
+    //   app: 'enroll',
+    //   user_name: 'kvootla',
+    //   org: 'maine',
+    //   repo: 'enroll',
+    //   commit_sha: 'abc1234',
+    // });
   }).timeout(5000);
 });
