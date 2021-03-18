1ca2b306967830a6ae0bcfdd7aaa08d45978908c
Mark Goho <markgoho@gmail.com>
2021-03-18 17:05:37 -0400

    trigger deployment

--- apps/dashboard/src/app/app-shell/app-shell.component.ts
+++ apps/dashboard/src/app/app-shell/app-shell.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   templateUrl: './app-shell.component.html',
   styleUrls: ['./app-shell.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppShellComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class AppShellComponent {}
