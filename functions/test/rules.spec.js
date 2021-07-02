f99268e8d982cfaae242806496a7b0cc5a4af91a
Mark Goho <markgoho@gmail.com>
2021-07-02 14:53:00 -0400

    refactor firestore rules tests

--- functions/test/rules.spec.js
+++ functions/test/rules.spec.js
@@ -5,22 +5,35 @@ const DEMO_FIREBASE_PROJECT_ID = 'demo-project';
 const port = require('../../firebase.json').emulators.firestore.port || 8080;
 const coverageUrl = `http://localhost:${port}/emulator/v1/projects/${DEMO_FIREBASE_PROJECT_ID}:ruleCoverage.html`;
 
-const philAuth = {
-  uid: 'phil',
-  email: 'phil@example.com',
-};
-
-const philUser = {
-  orgs: ['dchbx', 'health-connector'],
+const testUser1 = {
+  uid: 'test-user',
+  email: 'test-user@example.com',
+  orgs: ['org1', 'org2'],
   role: 'external',
 };
 
-const lemAuth = { uid: 'lem', email: 'lem@example.com' };
-
-const lemUser = {
+const ideaCrewUser = {
+  uid: 'ideacrew',
+  email: 'user@ideacrew.com',
   orgs: ['ideacrew'],
   role: 'admin',
 };
+
+const getFirestore = ({ uid, email }) =>
+  firebase
+    .initializeTestApp({
+      projectId: DEMO_FIREBASE_PROJECT_ID,
+      auth: { uid, email },
+    })
+    .firestore();
+
+const admin = firebase
+  .initializeAdminApp({
+    projectId: DEMO_FIREBASE_PROJECT_ID,
+  })
+  .firestore();
+
+const addUser = user => admin.doc(`users/${user.uid}`).set(user);
 
 before(async () => {
   const rulesContent = fs.readFileSync(
@@ -34,105 +47,107 @@ before(async () => {
 });
 
 describe('testing assertions', () => {
-  const noUserDb = firebase
-    .initializeTestApp({
-      projectId: DEMO_FIREBASE_PROJECT_ID,
-      auth: null,
-    })
-    .firestore();
+  const testUserDb = getFirestore(testUser1);
+  const ideaCrewDb = getFirestore(ideaCrewUser);
+  const testUserOrg = testUser1.orgs[0];
 
-  const philDb = firebase
-    .initializeTestApp({
-      projectId: DEMO_FIREBASE_PROJECT_ID,
-      auth: philAuth,
-    })
-    .firestore();
-
-  const lemDb = firebase
-    .initializeTestApp({
-      projectId: DEMO_FIREBASE_PROJECT_ID,
-      auth: lemAuth,
-    })
-    .firestore();
-
-  const admin = firebase
-    .initializeAdminApp({
-      projectId: DEMO_FIREBASE_PROJECT_ID,
-    })
-    .firestore();
-
-  const branchDoc = 'branches/feature-2.1';
-
-  it(`allows anyone to read all branches`, async () => {
-    await firebase.assertSucceeds(philDb.doc(branchDoc).get());
-    await firebase.assertSucceeds(lemDb.doc(branchDoc).get());
+  beforeEach(async () => {
+    await firebase.clearFirestoreData({ projectId: DEMO_FIREBASE_PROJECT_ID });
+    addUser(testUser1);
+    addUser(ideaCrewUser);
   });
 
-  it(`can only update a branch if logged in`, async () => {
+  it(`should only allow admins to read from branches`, async () => {
+    const branchDoc = 'branches/branch1';
     await admin.doc(branchDoc).set({ content: 'before' });
 
-    await firebase.assertSucceeds(
-      philDb.doc(branchDoc).update({ content: 'after' }),
-    );
-    await firebase.assertSucceeds(
-      lemDb.doc(branchDoc).update({ content: 'after' }),
-    );
+    await firebase.assertFails(testUserDb.doc(branchDoc).get());
+    await firebase.assertSucceeds(ideaCrewDb.doc(branchDoc).get());
+  });
+
+  it(`should only allow admins to update a branch doc`, async () => {
+    const branchDoc = 'branches/branch1';
+    await admin.doc(branchDoc).set({ content: 'before' });
+
     await firebase.assertFails(
-      noUserDb.doc(branchDoc).update({ content: 'after' }),
+      testUserDb.doc(branchDoc).update({ content: 'after' }),
     );
-  });
-
-  it(`can read and write an org if an admin`, async () => {
-    const orgDoc = 'orgs/dchbx';
-    await admin.doc('users/lem').set(lemUser);
-    await admin.doc(orgDoc).set({ content: 'before' });
-
-    await firebase.assertSucceeds(lemDb.doc(orgDoc).get());
     await firebase.assertSucceeds(
-      lemDb.doc(orgDoc).update({ content: 'after' }),
+      ideaCrewDb.doc(branchDoc).update({ content: 'after' }),
     );
   });
 
-  it(`can read but not write an org if member of org`, async () => {
-    const orgDoc = 'orgs/dchbx';
-    await admin.doc('users/phil').set(philUser);
+  it(`should allow members of org or admins to read org doc`, async () => {
+    const orgDoc = `orgs/${testUserOrg}`;
     await admin.doc(orgDoc).set({ content: 'before' });
 
-    await firebase.assertSucceeds(philDb.doc(orgDoc).get());
-    await firebase.assertFails(philDb.doc(orgDoc).update({ content: 'after' }));
+    await firebase.assertSucceeds(ideaCrewDb.doc(orgDoc).get());
+    await firebase.assertSucceeds(
+      ideaCrewDb.doc(orgDoc).update({ content: 'after' }),
+    );
+  });
+
+  it(`should only allow admins to write an org doc`, async () => {
+    const orgDoc = `orgs/${testUserOrg}`;
+    await admin.doc(orgDoc).set({ content: 'before' });
+
+    await firebase.assertFails(
+      testUserDb.doc(orgDoc).update({ content: 'after' }),
+    );
+    await firebase.assertSucceeds(
+      ideaCrewDb.doc(orgDoc).update({ content: 'after' }),
+    );
   });
 
   it('can read and write to environment docs if admin', async () => {
-    const envDoc = 'orgs/dchbx/environments/env-1';
-    await admin.doc('users/lem').set(lemUser);
+    const envDoc = `orgs/${testUserOrg}/environments/env-1`;
     await admin.doc(envDoc).set({ content: 'before' });
 
-    await firebase.assertSucceeds(lemDb.doc(envDoc).get());
+    await firebase.assertSucceeds(ideaCrewDb.doc(envDoc).get());
     await firebase.assertSucceeds(
-      lemDb.doc(envDoc).update({ content: 'after' }),
+      ideaCrewDb.doc(envDoc).update({ content: 'after' }),
     );
   });
 
-  it('can read but not write to environment docs if member of org', async () => {
-    const envDoc = 'orgs/dchbx/environments/env-1';
-    await admin.doc('users/phil').set(philUser);
+  it('should allow read but not write to environment docs if member of org', async () => {
+    const envDoc = `orgs/${testUserOrg}/environments/env-1`;
+    // await admin.doc('users/dchbx').set(dcbhxUser);
     await admin.doc(envDoc).set({ content: 'before' });
 
-    await firebase.assertSucceeds(philDb.doc(envDoc).get());
-    await firebase.assertFails(philDb.doc(envDoc).update({ content: 'after' }));
+    await firebase.assertSucceeds(testUserDb.doc(envDoc).get());
+    await firebase.assertFails(
+      testUserDb.doc(envDoc).update({ content: 'after' }),
+    );
+  });
+
+  it('can read environment services if member of org', async () => {
+    const serviceDoc = `orgs/${testUserOrg}/environments/env1/services/service1`;
+    await admin.doc(serviceDoc).set({ content: 'before' });
+
+    await firebase.assertSucceeds(testUserDb.doc(serviceDoc).get());
+    await firebase.assertSucceeds(ideaCrewDb.doc(serviceDoc).get());
+    await firebase.assertFails(
+      testUserDb.doc(serviceDoc).update({ content: 'after' }),
+    );
   });
 
   it('can only read user documents if logged in and match uid', async () => {
-    const userDoc = 'users/phil';
-    await admin.doc(userDoc).set({ content: 'before' });
+    const testUserDoc = `users/${testUser1.uid}`;
 
-    await firebase.assertSucceeds(philDb.doc(userDoc).get());
-    await firebase.assertFails(lemDb.doc(userDoc).get());
-    await firebase.assertFails(noUserDb.doc(userDoc).get());
+    await firebase.assertSucceeds(testUserDb.doc(testUserDoc).get());
+    await firebase.assertFails(ideaCrewDb.doc(testUserDoc).get());
+  });
+
+  it('can only read pull requests if admin', async () => {
+    const prDoc = 'pullRequests/pr1';
+
+    await firebase.assertFails(testUserDb.doc(prDoc).get());
+    await firebase.assertSucceeds(ideaCrewDb.doc(prDoc).get());
   });
 });
 
 after(async () => {
+  await firebase.clearFirestoreData({ projectId: DEMO_FIREBASE_PROJECT_ID });
   await Promise.all(firebase.apps().map(app => app.delete()));
   console.log(`View rule coverage information at ${coverageUrl}\n`);
 });
