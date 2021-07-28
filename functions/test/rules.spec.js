36ae9d2fab531ef616c4b9d928e8041fce18c6fb
Mark Goho <markgoho@gmail.com>
2021-07-28 15:25:24 -0400

    working to uncover bug

--- functions/test/rules.spec.js
+++ functions/test/rules.spec.js
@@ -52,12 +52,12 @@ describe('testing assertions', () => {
   const testUserOrg = testUser1.orgs[0];
 
   beforeEach(async () => {
-    await firebase.clearFirestoreData({ projectId: DEMO_FIREBASE_PROJECT_ID });
+    // await firebase.clearFirestoreData({ projectId: DEMO_FIREBASE_PROJECT_ID });
     addUser(testUser1);
     addUser(ideaCrewUser);
   });
 
-  it(`should only allow admins to read from branches`, async () => {
+  xit(`should only allow admins to read from branches`, async () => {
     const branchDoc = 'branches/branch1';
     await admin.doc(branchDoc).set({ content: 'before' });
 
@@ -65,7 +65,7 @@ describe('testing assertions', () => {
     await firebase.assertSucceeds(ideaCrewDb.doc(branchDoc).get());
   });
 
-  it(`should only allow admins to update a branch doc`, async () => {
+  xit(`should only allow admins to update a branch doc`, async () => {
     const branchDoc = 'branches/branch1';
     await admin.doc(branchDoc).set({ content: 'before' });
 
@@ -77,7 +77,7 @@ describe('testing assertions', () => {
     );
   });
 
-  it(`should allow members of org or admins to read org doc`, async () => {
+  xit(`should allow members of org or admins to read org doc`, async () => {
     const orgDoc = `orgs/${testUserOrg}`;
     await admin.doc(orgDoc).set({ content: 'before' });
 
@@ -87,7 +87,7 @@ describe('testing assertions', () => {
     );
   });
 
-  it(`should only allow admins to write an org doc`, async () => {
+  xit(`should only allow admins to write an org doc`, async () => {
     const orgDoc = `orgs/${testUserOrg}`;
     await admin.doc(orgDoc).set({ content: 'before' });
 
@@ -99,7 +99,7 @@ describe('testing assertions', () => {
     );
   });
 
-  it('can read and write to environment docs if admin', async () => {
+  xit('can read and write to environment docs if admin', async () => {
     const envDoc = `orgs/${testUserOrg}/environments/env-1`;
     await admin.doc(envDoc).set({ content: 'before' });
 
@@ -109,9 +109,8 @@ describe('testing assertions', () => {
     );
   });
 
-  it('should allow read but not write to environment docs if member of org', async () => {
+  xit('should allow read but not write to environment docs if member of org', async () => {
     const envDoc = `orgs/${testUserOrg}/environments/env-1`;
-    // await admin.doc('users/dchbx').set(dcbhxUser);
     await admin.doc(envDoc).set({ content: 'before' });
 
     await firebase.assertSucceeds(testUserDb.doc(envDoc).get());
@@ -124,21 +123,21 @@ describe('testing assertions', () => {
     const serviceDoc = `orgs/${testUserOrg}/environments/env1/services/service1`;
     await admin.doc(serviceDoc).set({ content: 'before' });
 
-    // await firebase.assertSucceeds(testUserDb.doc(serviceDoc).get());
-    await firebase.assertSucceeds(ideaCrewDb.doc(serviceDoc).get());
+    await firebase.assertSucceeds(testUserDb.doc(serviceDoc).get());
+    // await firebase.assertSucceeds(ideaCrewDb.doc(serviceDoc).get());
     // await firebase.assertFails(
     //   testUserDb.doc(serviceDoc).update({ content: 'after' }),
     // );
   });
 
-  it('can only read user documents if logged in and match uid', async () => {
+  xit('can only read user documents if logged in and match uid', async () => {
     const testUserDoc = `users/${testUser1.uid}`;
 
     await firebase.assertSucceeds(testUserDb.doc(testUserDoc).get());
     await firebase.assertFails(ideaCrewDb.doc(testUserDoc).get());
   });
 
-  it('can only read pull requests if admin', async () => {
+  xit('can only read pull requests if admin', async () => {
     const prDoc = 'pullRequests/pr1';
 
     await firebase.assertFails(testUserDb.doc(prDoc).get());
@@ -147,7 +146,7 @@ describe('testing assertions', () => {
 });
 
 after(async () => {
-  await firebase.clearFirestoreData({ projectId: DEMO_FIREBASE_PROJECT_ID });
-  await Promise.all(firebase.apps().map(app => app.delete()));
-  console.log(`View rule coverage information at ${coverageUrl}\n`);
+  // await firebase.clearFirestoreData({ projectId: DEMO_FIREBASE_PROJECT_ID });
+  // await Promise.all(firebase.apps().map(app => app.delete()));
+  // console.log(`View rule coverage information at ${coverageUrl}\n`);
 });
