c7a9669471cb7f7bacdc1e4521c8e0dbb149f9e7
Mark Goho <markgoho@gmail.com>
2021-07-28 16:58:34 -0400

    trigger run

--- functions/test/rules.spec.js
+++ functions/test/rules.spec.js
@@ -57,7 +57,7 @@ describe('testing assertions', () => {
     addUser(ideaCrewUser);
   });
 
-  xit(`should only allow admins to read from branches`, async () => {
+  it(`should only allow admins to read from branches`, async () => {
     const branchDoc = 'branches/branch1';
     await admin.doc(branchDoc).set({ content: 'before' });
 
