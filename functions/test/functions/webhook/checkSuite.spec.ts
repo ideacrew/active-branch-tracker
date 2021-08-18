30b6b923d68a85f16e031f5ae2c0e26850bd241d
Mark Goho <markgoho@gmail.com>
2021-08-18 16:52:40 -0400

    purposely fail test

--- functions/test/functions/webhook/checkSuite.spec.ts
+++ functions/test/functions/webhook/checkSuite.spec.ts
@@ -100,7 +100,7 @@ describe('Check Suite tests', () => {
     expect(checkSuiteSnapshot.data()).to.include({
       checkSuiteStatus: 'failure',
       checkSuiteRuns: 2,
-      checkSuiteFailures: 1,
+      checkSuiteFailures: 2,
     });
   }).timeout(5000);
 });
