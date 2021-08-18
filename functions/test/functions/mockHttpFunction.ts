802bb2fa07485676b6560f6261b9cf70222b5b5d
Mark Goho <markgoho@gmail.com>
2021-08-18 16:30:30 -0400

    add delete collection utility function (#22)
    * change env var name
    
    * add delete collection utility function

--- functions/test/functions/mockHttpFunction.ts
+++ functions/test/functions/mockHttpFunction.ts
@@ -3,7 +3,7 @@ import axios, { AxiosRequestConfig } from 'axios';
 const axiosConfig = (eventType: string, data: unknown): AxiosRequestConfig => {
   return {
     method: 'post',
-    url: `http://localhost:5001/${process.env.GCLOUD_PROJECT}/us-central1/webhook`,
+    url: `http://localhost:5001/${process.env.GCP_PROJECT}/us-central1/webhook`,
     headers: {
       'Content-Type': 'application/json',
       'X-Github-Event': eventType,
