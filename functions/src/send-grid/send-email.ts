981a77f105a0e623934a222ea12b3372c6b023fa
Mark Goho <markgoho@gmail.com>
2021-10-19 11:41:37 -0400

    update secondary dependencies

--- functions/src/send-grid/send-email.ts
+++ functions/src/send-grid/send-email.ts
@@ -1,12 +1,12 @@
-import { MailDataRequired, send, setApiKey } from '@sendgrid/mail';
-import * as functions from 'firebase-functions';
+// import { MailDataRequired, send, setApiKey } from '@sendgrid/mail';
+// import * as functions from 'firebase-functions';
 
 /**
  * Sends the email
  * @param {MailDataRequired} email
  */
-export async function sendMail(email: MailDataRequired): Promise<void> {
-  setApiKey(functions.config().sendgrid.key);
+// export async function sendMail(email: MailDataRequired): Promise<void> {
+//   setApiKey(functions.config().sendgrid.key);
 
-  await send(email);
-}
+//   await send(email);
+// }
