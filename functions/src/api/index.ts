d541d72cd177ac3f4e2c3014fc1f02541354ee4c
Mark Goho <markgoho@gmail.com>
2021-12-06 18:03:35 -0500

    update curl request

--- functions/src/api/index.ts
+++ functions/src/api/index.ts
@@ -6,11 +6,13 @@ import { serviceDeployment, testRoute } from './controllers';
 const apiPrefix = 'api';
 export const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
+  functions.logger.info('Request:', req.body);
+
   if (req.url.indexOf(`/${apiPrefix}/`) === 0) {
-    functions.logger.log('Request url', req.url);
     req.url = req.url.substring(apiPrefix.length + 1);
-    functions.logger.log('New request url', req.url);
   }
 
   next();
