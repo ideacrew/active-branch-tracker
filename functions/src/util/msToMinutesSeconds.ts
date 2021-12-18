1dc7268147221b71b5ab609e853da50c5c859b8b
Mark Goho <markgoho@gmail.com>
2021-12-17 22:36:41 -0500

    update time diff function

--- functions/src/util/msToMinutesSeconds.ts
+++ functions/src/util/msToMinutesSeconds.ts
@@ -1,5 +1,7 @@
 export const msToMinutesAndSeconds = (ms: number): string => {
   const minutes = Math.floor(ms / 60000);
   const seconds = parseInt(((ms % 60000) / 1000).toFixed(0), 10);
-  return `${minutes}m ${seconds < 10 ? '0' : ''}s`;
+  const runtime = `${minutes}m ${seconds}s`;
+
+  return runtime;
 };
