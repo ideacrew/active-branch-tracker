97f743c785559f96d857abea1a96c232f077b84c
Mark Goho <markgoho@gmail.com>
2022-04-29 13:14:54 -0400

    remove app-shell component

--- apps/dashboard/src/app/app-shell/app-shell.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({
-  templateUrl: './app-shell.component.html',
-  styleUrls: ['./app-shell.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class AppShellComponent {}
