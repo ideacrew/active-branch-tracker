b07d047c36000084346a872f90e1884c714e53ec
Mark Goho <markgoho@gmail.com>
2022-06-30 16:16:35 +0000

    return default value when looking up team

--- functions/src/util/get-team-membership.ts
+++ functions/src/util/get-team-membership.ts
@@ -7,6 +7,9 @@ export const getTeamMembership = async (
 ): Promise<Team | undefined> => {
   const database = firestore();
 
+  // Assuming this runs in an environment where an author
+  // collection exists and has some info about which team
+  // the author is on
   const authorSnapshot = await database
     .collection('authors')
     .doc(userName)
@@ -14,5 +17,6 @@ export const getTeamMembership = async (
 
   const authorDocument = authorSnapshot.data() as FSAuthor;
 
-  return authorDocument.team;
+  // In case this runs in CI, return a default value
+  return authorDocument.team ?? 'ideacrew';
 };
