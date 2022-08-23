5d1ab3ba907202ff078478dde2b2a2cecc300164
Mark Goho <markgoho@gmail.com>
2022-08-23 22:41:35 +0000

    update regex for container registry

--- functions/src/service-deployment/handle-service-deployment.ts
+++ functions/src/service-deployment/handle-service-deployment.ts
@@ -1,4 +1,5 @@
 import * as admin from 'firebase-admin';
+import { logger } from 'firebase-functions';
 
 import { ImageInfo, ServiceDeploymentPayload } from '../api/models';
 import {
@@ -13,6 +14,7 @@ import { parseImage } from './utils';
 export const handleServiceDeployment = async (
   payload: ServiceDeploymentPayload,
 ): Promise<{ status: string; message: unknown }> => {
+  logger.info(`Received service deployment: ${payload.image}`);
   const imageInfo: ImageInfo = parseImage(payload.image);
 
   const { repo: repository } = imageInfo;
--- functions/src/service-deployment/utils/parse-image.ts
+++ functions/src/service-deployment/utils/parse-image.ts
@@ -13,7 +13,7 @@ export const parseImage = (
   image = 'public.ecr.aws/ideacrew/fake:fake-abc1234',
 ): ImageInfo => {
   const regex =
-    /(?<registry>public.ecr.aws)\/(?<owner>ideacrew)\/(?<repo>[a-z]+):(?<branch>[\d._a-z-]+)-(?<commit_sha>[\da-f]{7})-?(?:[c-em]{2})?/gm;
+    /(?<registry>public\.ecr\.aws|ghcr\.io)\/(?<owner>ideacrew)\/(?<repo>[a-z]+):(?<branch>[\d._a-z-]+)-(?<commit_sha>[\da-f]{7})-?(?:[c-em]{2})?/gm;
 
   // Provide fallback in case image can't be matched
   const { groups } = regex.exec(image) ?? {
